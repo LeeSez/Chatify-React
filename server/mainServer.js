b755d5c8eed5e5fc76c3bd3420d2940178f40511
LeeSez <109472918+LeeSez@users.noreply.github.com>
2023-04-11 15:28:03 +0300

    Contacts are now updating well and according to the last message 's time
    In the previous versions, the contacts used to update based on the sender of the message only, meaning that if I had sent a message to some new, hew would not appear as a chat. Now, if I send a message to someone it is treated as the last message from that person and therefor, will be up top.

--- server/mainServer.js
+++ server/mainServer.js
@@ -252,10 +252,26 @@ function verifyLogin(res, connection, successCallback){ //responsible for verify
     })
 }
 
+
 function pullContacts(res,messages, lastId, connection){ //resposible for pulling the contact by the most relevent contacts
-    connection.query("SELECT table1.sender as email, users.name, users.profile_picture FROM (SELECT MAX(time) AS last_message_time, sender AS sender FROM messages WHERE sender=? OR recipient=? GROUP BY sender) AS table1 INNER JOIN users ON users.email = table1.sender ORDER BY last_message_time DESC;", [email, email], (error1, result1)=>{
-        
-        let finishilizing = ()=>{
+    connection.query("SELECT u.email, u.name, u.profile_picture, m.content, m.time FROM users u JOIN ( SELECT MAX(id) AS last_message_id, CASE WHEN sender = ? THEN recipient WHEN recipient = ? THEN sender END AS contact_email FROM messages JOIN users s ON s.email = messages.sender JOIN users r ON r.email = messages.recipient WHERE s.email = ? OR r.email = ? GROUP BY contact_email ) l ON l.contact_email = u.email JOIN messages m ON m.id = l.last_message_id ORDER BY m.time DESC;", [email,email,email,email],(error1, result1)=>{
+        if(error1){
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.write("run into problem while trying to pull the contacts");
+            res.end();
+            return;
+        }
+
+        // retriving personal information
+        connection.query("SELECT * FROM users WHERE email=?",[email],(error2,result2)=>{
+            connection.end();
+            if(error2){
+                res.writeHead(200,{'Content-Type':'text/plain'});
+                res.write("the server encountered an issue while trying to retrive the user's information");
+                res.end();
+            }
+            let myDetailes = result2[0];
+            
             for(let i = 0; i<result1.length; i++){
                 let lastmessage = serverTools.findLast(messages,(mess) =>{return (mess.sender==result1[i].email || mess.recipient==result1[i].email)});
                 result1[i].lastmessage = lastmessage;
@@ -272,38 +288,11 @@ function pullContacts(res,messages, lastId, connection){ //resposible for pullin
             res.writeHead(200, {'Content-Type':'application/JSON'});
             res.end(JSON.stringify(response));
             return;
-        }
+        });
 
-        if(error1){
-            res.writeHead(500, {'Content-Type': 'text/plain'});
-            res.write("run into problem while trying to query the database");
-            res.end();
-            return;
-        }
-
-        let indexOfMyEmail = findIndexByfield("email", email, result1);
-        let myDetailes = {};
-        if(indexOfMyEmail != -1){
-            myDetailes = result1.splice(indexOfMyEmail,1);
-            myDetailes = myDetailes[0];
-            connection.end();
-            finishilizing();
-        }
-        else{
-            // email not found or there might be no messages an therefor no contacts.
-            connection.query("SELECT * FROM users WHERE email=?",[email],(error2,result2)=>{
-                connection.end();
-                if(error2){
-                    res.writeHead(200,{'Content-Type':'text/plain'});
-                    res.write("the server encountered an issue while trying to retrive the users information");
-                    res.end();
-                }
-                myDetailes = result2[0];
-                finishilizing();
-            });
-        }
     });
 }
+
 
 function findIndexByfield(fieldName, val, array) { //only meant to find object's position according to a spesified field and val
     for (let i = 0; i < array.length; i++) {
